68caab023976c95a021a586b63e20488fbfd3c22
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-06-26 13:12:09 +0300

    fix push

--- server/app.py
+++ server/app.py
@@ -1,26 +1,45 @@
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_migrate import Migrate
-
-from models import db, Message
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+db = SQLAlchemy(app)
 
-CORS(app)
-migrate = Migrate(app, db)
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.String)
+    updated_at = db.Column(db.DateTime)
 
-db.init_app(app)
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.serialize() for message in messages])
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    body = request.form.get('body')
+    username = request.form.get('username')
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.serialize())
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    body = request.form.get('body')
+    message.body = body
+    db.session.commit()
+    return jsonify(message.serialize())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'})
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run()
